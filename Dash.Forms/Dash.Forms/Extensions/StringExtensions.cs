aa0629c047cd116019540f48d7db4718af47a61f
nieminen <nieminen432@gmail.com>
2019-02-24 14:39:07 -0600

    Added string extensions class;

---
+++ Dash.Forms/Dash.Forms/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Dash.Forms.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
