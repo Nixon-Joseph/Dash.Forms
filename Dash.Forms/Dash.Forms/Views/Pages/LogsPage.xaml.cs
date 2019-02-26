eb6909ba8385683c2c87e2a4fe56b77b41407111
nieminen <nieminen432@gmail.com>
2019-02-25 22:20:05 -0600

    Added simple framework for displaying run logs

--- Dash.Forms/Dash.Forms/Views/Pages/LogsPage.xaml.cs
+++ Dash.Forms/Dash.Forms/Views/Pages/LogsPage.xaml.cs
@@ -12,9 +12,43 @@ namespace Dash.Forms.Views.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LogsPage : ContentPage
     {
+        public IEnumerable<LogGroup> Logs = new List<LogGroup>() {
+            new LogGroup("Week 1", "1") { new Log(), new Log() },
+            new LogGroup("Week 2", "2") { new Log(), new Log() },
+            new LogGroup("Week 3", "3") { new Log() },
+            new LogGroup("Week 4", "4") { new Log(), new Log() },
+            new LogGroup("Week 5", "5") { new Log(), new Log() },
+            new LogGroup("Week 6", "6") { new Log(), new Log() },
+            new LogGroup("Week 7", "7") { new Log(), new Log() }
+        };
+
         public LogsPage()
         {
             InitializeComponent();
+
+            LogListView.ItemsSource = Logs;
+            LogListView.ItemSelected += LogListView_ItemSelected;
         }
+
+        private void LogListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            LogListView.SelectedItem = null;
+        }
+    }
+
+    public class LogGroup : List<Log>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; }
+        public LogGroup(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+    }
+
+    public class Log
+    {
+
     }
 }
