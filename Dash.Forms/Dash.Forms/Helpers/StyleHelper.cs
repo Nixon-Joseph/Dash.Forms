cc0ad678e50ea4d53e2ee07bfc43f93cc1205b26
Cody <gibbsinater@gmail.com>
2019-02-28 21:59:17 -0600

    Fixed error on logs page. Updated accent color. Set launch screen to use a gradient background color.

--- Dash.Forms/Dash.Forms/Helpers/StyleHelper.cs
+++ Dash.Forms/Dash.Forms/Helpers/StyleHelper.cs
@@ -14,7 +14,7 @@ namespace Dash.Forms.Helpers
     {
         public static readonly string Primary = "8cc34b";
         public static readonly string PrimaryDark = "689f3a";
-        public static readonly string Accent = "4484ff";
+        public static readonly string Accent = "05668D";
         public static readonly string Black = "040404";
         public static readonly string White = "FFFFFF";
         public static readonly string OffWhite = "EBF2FA";
