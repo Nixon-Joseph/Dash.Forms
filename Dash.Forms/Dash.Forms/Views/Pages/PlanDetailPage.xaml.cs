f157ff696c6b74b3cf9e45693a22f11daff8c04c
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-07-15 21:28:59 -0500

    [finishes #161495191] Added ability to start a  planned run from run plan pages;

--- Dash.Forms/Dash.Forms/Views/Pages/PlanDetailPage.xaml.cs
+++ Dash.Forms/Dash.Forms/Views/Pages/PlanDetailPage.xaml.cs
@@ -11,21 +11,30 @@ namespace Dash.Forms.Views.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PlanDetailPage : ContentPage
     {
+        private TrainingPlan Plan;
+        private IEnumerable<PlanWeekContainer> Weeks;
+
         public PlanDetailPage(TrainingPlan plan)
         {
             InitializeComponent();
+            Plan = plan;
             BindingContext = plan;
 
-            PlanList.ItemsSource = plan.Weeks.Select(w => new PlanWeekContainer(w));
+            Weeks = plan.Weeks.Select(w => new PlanWeekContainer(w));
+            PlanList.ItemsSource = Weeks;
             PlanList.ItemSelected += PlanList_ItemSelected;
         }
 
-        private void PlanList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private async void PlanList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             PlanList.SelectedItem = null;
             if (e.SelectedItem is TrainingDay day)
             {
-
+                if (await DisplayAlert("Start Run?", $"Would you like to start this run?", "Lets go!", "Nope") == true)
+                {
+                    var week = Weeks.FirstOrDefault(w => w.Contains(day));
+                    await Navigation.PushAsync(new RunTabbedPage(day, Plan.Id, week?.WeekNumber ?? 0));
+                }
             }
         }
 
