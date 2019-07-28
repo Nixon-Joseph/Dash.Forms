415c38f9f2a23cc84313d0e7d296cee2d8db1932
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-07-28 13:46:38 -0500

    Updated chart to use custom package (broke build)

--- Dash.Forms/Dash.Forms/Views/Pages/MetricsPage.xaml.cs
+++ Dash.Forms/Dash.Forms/Views/Pages/MetricsPage.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dash.Forms.Extensions;
 using Dash.Forms.Helpers;
 using Dash.Forms.Helpers.Storage;
-using Microcharts;
-using Microcharts.Forms;
 using SkiaSharp;
+using Xamarin.Cross.Charts;
+using Xamarin.Cross.Charts.Charts;
+using Xamarin.Cross.Charts.Enums;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Dash.Forms.Extensions;
 
 namespace Dash.Forms.Views.Pages
 {
@@ -56,24 +52,24 @@ namespace Dash.Forms.Views.Pages
                 LabelOrientation = Orientation.Horizontal,
                 ValueLabelOrientation = Orientation.Horizontal,
                 LabelTextSize = 60f,
-                Entries = new[]
+                Inputs = new[]
                 {
-                    new ChartEntry(200)
+                    new ChartInput(200)
                     {
                         Label = "January",
-                        ValueLabel = "200",
+                        DisplayValue = "200",
                         Color = SKColor.Parse("#266489")
                     },
-                    new ChartEntry(400)
+                    new ChartInput(400)
                     {
                         Label = "February",
-                        ValueLabel = "400",
+                        DisplayValue = "400",
                         Color = SKColor.Parse("#68B9C0")
                     },
-                    new ChartEntry(-100)
+                    new ChartInput(-100)
                     {
                         Label = "March",
-                        ValueLabel = "-100",
+                        DisplayValue = "-100",
                         Color = SKColor.Parse("#90D585")
                     }
                 }
