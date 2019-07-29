5f46dc5c66714c8177ee031f65612c2309a514de
Joseph Nieminen Nixon <nieminen432@gmail.com>
2019-07-29 14:42:51 -0500

    Created android and iOS wearable applications;

---
+++ Dash.Forms.watchOS/Dash.Forms.watchOS.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace Dash.Forms.watchOS.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
