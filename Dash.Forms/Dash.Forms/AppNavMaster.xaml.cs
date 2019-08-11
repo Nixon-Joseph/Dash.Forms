95308eba1b3830d0c70dc6d4a7c859cf867cee68
Cody <gibbsinater@gmail.com>
2019-08-11 15:57:59 -0500

    Improved Styling for Run setup. Changed "Quick Run" to "Run" in the navigation. Added a "Quick Run" option to the navigation that immediately starts a run.

--- Dash.Forms/Dash.Forms/AppNavMaster.xaml.cs
+++ Dash.Forms/Dash.Forms/AppNavMaster.xaml.cs
@@ -39,8 +39,8 @@ namespace Dash.Forms
                 MenuItems = new ObservableCollection<IAppNavMenuItem>(new IAppNavMenuItem[]
                 {
                     new AppNavMenuItem<HomePage> { Title = "Home", Icon = "menu_home.png" },
-                    new AppNavMenuItem<QuickRunStartupPage> { Title = "Quick Run", Icon = "menu_run.png" },
-                    //new AppNavMenuItem<RunPage> { Title = "Free Run", Icon = "menu_run.png" },
+                    new AppNavMenuItem<QuickRunStartupPage> { Title = "Run", Icon = "menu_run.png" },
+                    new AppNavMenuItem<RunTabbedPage> { Title = "Quick Run", Icon = "menu_run.png" },
                     new AppNavMenuItem<LogsPage> { Title = "Logs", Icon = "menu_logs.png" },
                     new AppNavMenuItem<PlansPage> { Title = "Plans", Icon = "menu_plan.png" },
                     new AppNavMenuItem<MetricsPage> { Title = "Metrics", Icon = "menu_metrics.png" },
